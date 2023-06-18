f622aa6571c6f9a2b2ee0742f9552c35cbecc792
matheusqsa <matheusqsa123@gmail.com>
2023-06-18 19:50:14 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle('light')
+    
+    // //pegar a tag img
+    // const img = document.querySelector("#profile img")
+
+    // //substituir a imagem
+    //  if(html.classList.contains('light')){
+    // // se tiver light toggleMode, adicionar a imagem light
+    //     img.setAttribute('src', 'assets/eu-light.png')
+    //  } else {
+    //     img.setAttribute('src','assets/eu-mesmo-crop.png')
+    // // se tiver sem light toggleMode, manter a imagem normal
+
+    //  }
+    
+
+}
