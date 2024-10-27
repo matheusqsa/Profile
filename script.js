3c4e7602334dcdce1bedd935b3f9eb31a5693ff9
Matheus <123829013+matheusqsa@users.noreply.github.com>
2024-10-26 22:43:20 -0300

    4º Change - Trying to configure Toggle Mode

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ function toggleMode() {
     html.classList.toggle('light')
     
     // //pegar a tag img
-    // const img = document.querySelector("#profile img")
+     const img = document.querySelector("#profile img")
 
     // //substituir a imagem
-    //  if(html.classList.contains('light')){
+      if(html.classList.contains('light')){
     // // se tiver light toggleMode, adicionar a imagem light
-    //     img.setAttribute('src', 'assets/eu-light.png')
-    //  } else {
-    //     img.setAttribute('src','assets/eu-mesmo-crop.png')
+         img.setAttribute('src', 'assets/eu-light.png')
+      } else {
+         img.setAttribute('src','assets/eu-mesmo-crop.png')
     // // se tiver sem light toggleMode, manter a imagem normal
 
-    //  }
+      }
     
 
 }
